a508f3057ae00bbb0db698cc492e750543d8125d
Vital-pro <vitupikov@yandex.ru>
2024-07-18 01:04:10 +0300

    second commit

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const cron = require('node-cron');
 const cheerio = require('cheerio');
+
+// cron.schedule('* * * * *', () => {
+//   console.log('running a task every minute');
+// });
 
 const app = express();
 
@@ -14,6 +19,17 @@ app.get('/api/news', (req, res) => {
   res.json({
     temp: 30,
   });
+});
+
+app.get('/password', (req, res) => {
+
+  if (req.query.password === process.env.PASSWORD) {
+    res.sendFile('./public/index1.html', { root: __dirname })
+  } else {
+    res.json({
+      authorized: 'Bad Request',
+    });
+  }
 });
 
 app.listen(process.env.PORT || 3001, () => {

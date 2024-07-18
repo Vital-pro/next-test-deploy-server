7c51836c4abadc05638c80bcbdd6c3574afc2f71
Vital-pro <vitupikov@yandex.ru>
2024-07-18 15:24:52 +0300

    modernisation

--- public/client.js
+++ public/client.js
@@ -1,17 +1,10 @@
-let formClient = document.getElementById('form');
-console.log(formClient);
+// let formClient = document.getElementById('form');
+// console.log(formClient);
 
-formClient.addEventListener('submit', () => {
-  let password = formClient.querySelector('input[name="password"]');
-  // event.preventDefault();
-  let result = fetch(`https://next-test-deploy-server.vercel.app/password/${password.value}`)
-    .then(response => response.json())
-    .then(data => console.log(data));
-})
-
-
-// formClient.querySelector('input[type="submit"]').addEventListener('click', (event) => {
-//   event.preventDefault();
-//   let password = formClient.querySelector('input[name="password"]').value;
-//   window.location.href = `/password/${password}`
+// formClient.addEventListener('submit', () => {
+//   let password = formClient.querySelector('input[name="password"]');
+//   // event.preventDefault();
+//   let result = fetch(`https://next-test-deploy-server.vercel.app/password/${password.value}`)
+//     .then(response => response.json())
+//     .then(data => console.log(data));
 // })

c56a72236d7453a2c7c9a2efa61af63a1e2427d4
Vital-pro <vitupikov@yandex.ru>
2024-07-18 22:36:34 +0300

    delete crontab

--- public/client.js
+++ public/client.js
@@ -1,13 +1,13 @@
 let formClient = document.getElementById('form');
 console.log(formClient);
 
-formClient.addEventListener('submit', async () => {
-  let password = document.getElementById('passw');
+formClient.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  // let password = document.getElementById('passw');
   // let result = await fetch(`https://next-test-deploy-server.vercel.app/password/${password.value}`)
   let dogImg = document.getElementById('img');
   let result = await fetch(`http://localhost:3001/api/news`)
     .then(response => response.json())
     // .then(data => console.log(data));
-  let div = document.getElementById('card');
   dogImg.src = result.message
 })
--- server.js
+++ server.js
@@ -15,16 +15,16 @@ app.use('/', express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const checkPassword = (req, res, next) => {
-  if (req.query.password !== process.env.PASSWORD) {
-    res.json({
-      authorized: 'Bad Request',
-    });
-    return;
-  } else {
-    next();
-  }
-};
+// const checkPassword = (req, res, next) => {
+//   if (req.query.password !== process.env.PASSWORD) {
+//     res.json({
+//       authorized: 'Bad Request',
+//     });
+//     return;
+//   } else {
+//     next();
+//   }
+// };
 
 async function getData() {
   const response = await fetch(`https://dog.ceo/api/breeds/image/random`);
@@ -34,29 +34,29 @@ async function getData() {
   return app.locals.data;
 }
 
-app.get('/api/first', (req, res) => {
+app.get('/api/first',async (req, res) => {
   // cron.schedule('* * * * *', getData)
+  const data = await getData();
   res.send('Hello World!');
 });
 
 app.get('/api/news', async (req, res) => {
-  const data = await getData();
   // const data = app.locals.data;
-  console.log(data)
+  // console.log(data)
   res.json({
     message: app.locals.data,
   });
 });
 
-app.get('/api/password', checkPassword, (req, res) => {
-  if (req.query.password === process.env.PASSWORD) {
-  res.sendFile('./public/index1.html', { root: __dirname });
-  } else {
-  res.json({
-    authorized: 'Bad Request',
-  });
-  }
-});
+// app.get('/api/password', checkPassword, (req, res) => {
+//   if (req.query.password === process.env.PASSWORD) {
+//   res.sendFile('./public/index1.html', { root: __dirname });
+//   } else {
+//   res.json({
+//     authorized: 'Bad Request',
+//   });
+//   }
+// });
 
 app.listen(process.env.PORT || 3001, () => {
   console.log(

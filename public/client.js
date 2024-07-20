cf2f4f82c4edf5bc18c935eadfa0461d4f67e8cc
Vital-pro <vitupikov@yandex.ru>
2024-07-20 14:16:27 +0300

    changed clock

--- public/client.js
+++ public/client.js
@@ -3,8 +3,9 @@ let formClient = document.getElementById('form');
 formClient.addEventListener('submit', async (e) => {
   e.preventDefault();
   // let result = await fetch(`https://next-test-deploy-server.vercel.app/api/news`)
-  // let result = await fetch(`http://http://45.142.36.120/api/news`)
-  let result = await fetch(`http://localhost:3001/api/news`).then((response) =>
+  let result = await fetch(`http://http://45.142.36.120/api/news`)
+  // let result = await fetch(`http://localhost:3001/api/news`)
+  .then((response) =>
     response.json()
   );
   let dogImg = document.getElementById('img');
